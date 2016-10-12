c831a765b2cdb7889975f2f375605c17e558f85b
bai <baiyalong@gmail.com>
2016-10-13 05:59:34 +0800

    table

--- imports/ui/Admin/User.js
+++ imports/ui/Admin/User.js
@@ -1,15 +1,44 @@
 import React, {Component, PropTypes} from 'react';
+import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table';
 
 
-class UserPage extends Component {
+class User extends Component {
     render() {
         return (
-            <div  >
-               '用户管理' 
-            </div>
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHeaderColumn>ID</TableHeaderColumn>
+                        <TableHeaderColumn>角色</TableHeaderColumn>
+                        <TableHeaderColumn>状态</TableHeaderColumn>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    <TableRow>
+                        <TableRowColumn>1</TableRowColumn>
+                        <TableRowColumn>John Smith</TableRowColumn>
+                        <TableRowColumn>Employed</TableRowColumn>
+                    </TableRow>
+                    <TableRow>
+                        <TableRowColumn>2</TableRowColumn>
+                        <TableRowColumn>Randal White</TableRowColumn>
+                        <TableRowColumn>Unemployed</TableRowColumn>
+                    </TableRow>
+                    <TableRow>
+                        <TableRowColumn>3</TableRowColumn>
+                        <TableRowColumn>Stephanie Sanders</TableRowColumn>
+                        <TableRowColumn>Employed</TableRowColumn>
+                    </TableRow>
+                    <TableRow>
+                        <TableRowColumn>4</TableRowColumn>
+                        <TableRowColumn>Steve Brown</TableRowColumn>
+                        <TableRowColumn>Employed</TableRowColumn>
+                    </TableRow>
+                </TableBody>
+            </Table>
         )
     }
 }
 
 
-export default UserPage
+export default User
--- imports/ui/Admin/Work.js
+++ imports/ui/Admin/Work.js
@@ -1,15 +1,47 @@
 import React, {Component, PropTypes} from 'react';
+import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table';
 
 
-class WorkPage extends Component {
+class Work extends Component {
     render() {
         return (
-            <div  style={{ height: '100%' }}>
-                '众创活动' 
-            </div>
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHeaderColumn>题目</TableHeaderColumn>
+                        <TableHeaderColumn>作者</TableHeaderColumn>
+                        <TableHeaderColumn>评分</TableHeaderColumn>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    <TableRow>
+                        <TableRowColumn>1</TableRowColumn>
+                        <TableRowColumn>John Smith</TableRowColumn>
+                        <TableRowColumn>Employed</TableRowColumn>
+                    </TableRow>
+                    <TableRow>
+                        <TableRowColumn>2</TableRowColumn>
+                        <TableRowColumn>Randal White</TableRowColumn>
+                        <TableRowColumn>Unemployed</TableRowColumn>
+                    </TableRow>
+                    <TableRow>
+                        <TableRowColumn>3</TableRowColumn>
+                        <TableRowColumn>Stephanie Sanders</TableRowColumn>
+                        <TableRowColumn>Employed</TableRowColumn>
+                    </TableRow>
+                    <TableRow>
+                        <TableRowColumn>4</TableRowColumn>
+                        <TableRowColumn>Steve Brown</TableRowColumn>
+                        <TableRowColumn>Employed</TableRowColumn>
+                    </TableRow>
+                </TableBody>
+            </Table>
         )
     }
 }
 
 
-export default WorkPage
+export default Work
+
+
+
